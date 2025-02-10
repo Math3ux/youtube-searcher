43a7f5292f53a26b134c35fd5c3057f22aef1444
Matheus <matheusleidow@gmail.com>
2025-02-10 20:48:09 -0300

    remoção de algumas classes desnecessárias

--- src/Components/Results.tsx
+++ src/Components/Results.tsx
@@ -22,7 +22,7 @@ export const Results = ({ term }: ResultsProps) => {
                         part: 'snippet',
                         q: term,
                         key: apiKey,
-                        maxResults: 10,
+                        maxResults: 50,
                         type: 'video'
                     }
                 });
@@ -42,10 +42,10 @@ export const Results = ({ term }: ResultsProps) => {
     if (loading) return <div>Carregando...</div>;
 
     return (
-        <div className='text-black'>
+        <div>
             {results.map((item) => (
-                <div key={item.id.videoId} className="flex flex-col space-y-4 items-center block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>{item.snippet.title}</h2>
+                <div key={item.id.videoId} className="m-3 flex flex-col items-center bg-gray-800">
+                    <h2 className='mb-2 text-2xl font-bold tracking-tight text-white'>{item.snippet.title}</h2>
                     <img src={item.snippet.thumbnails.default.url} alt={item.snippet.title} />
                     <p className='text-white'>https://www.youtube.com/watch?v={item.id.videoId}</p>
                 </div>
